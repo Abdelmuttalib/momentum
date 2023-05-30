e5fb2587020470740d6c0b2b52dd441c7cae58d0
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2023-05-30 21:36:40 +0800

    add comment

--- src/components/@pages/team/data-table.tsx
+++ src/components/@pages/team/data-table.tsx
@@ -97,7 +97,7 @@ export function DataTable<TData, TValue>({
         </Table>
       </div>
 
-      {/* Pagination */}
+      {/* Table Pagination */}
       <div className="flex items-center justify-end space-x-2 py-4">
         <Button
           variant="outline"
