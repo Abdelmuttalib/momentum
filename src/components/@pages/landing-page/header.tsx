ac5ea0c447617fceb8d11d424e609a3aa7add8f4
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2024-01-22 18:43:49 +0800

    Update landing page header styling

--- src/components/@pages/landing-page/header.tsx
+++ src/components/@pages/landing-page/header.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Container from "./container";
 import { ButtonLink } from "@/components/ui/button";
-import ThemeToggle from "@/components/theme-toggle";
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-10 border-b bg-transparent text-slate-800 backdrop-blur backdrop-filter transition-colors duration-300">
       <nav className="w-full">
         <Container>
-          <div className="relative flex flex-wrap items-center justify-between gap-6 py-2 md:gap-0">
+          <div className="relative flex flex-wrap items-center justify-between gap-6 py-4 md:gap-0">
             <input
               aria-hidden="true"
               type="checkbox"
@@ -34,7 +33,7 @@ export default function Header() {
                 </span>
               </a>
 
-              <div className="relative flex max-h-10 items-center bg-red-300 lg:hidden">
+              <div className="relative flex max-h-10 items-center lg:hidden">
                 <label
                   role="button"
                   htmlFor="toggle_nav"
@@ -58,7 +57,7 @@ export default function Header() {
             {/* overlay */}
             <div
               aria-hidden="true"
-              className="fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 bg-white/90 backdrop-blur-2xl transition duration-500 peer-checked:origin-top peer-checked:scale-y-100 dark:bg-gray-900/70 lg:hidden"
+              className="fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 bg-gray-900/90 backdrop-blur-2xl transition duration-500 peer-checked:origin-top peer-checked:scale-y-100 dark:bg-gray-900/70 lg:hidden"
             ></div>
             <div
               className="invisible absolute left-0 top-full z-20 w-full origin-top translate-y-0 scale-95 flex-col flex-wrap justify-end gap-6 py-8 opacity-0 shadow-gray-600/10 transition-all duration-300 
@@ -67,15 +66,11 @@ export default function Header() {
                             lg:opacity-100 lg:shadow-none lg:peer-checked:translate-y-0"
             >
               <div className="w-full text-gray-500 dark:text-gray-300 lg:w-auto lg:pr-4 lg:pt-0">
-                <ul className="flex flex-col gap-2 font-semibold tracking-normal lg:flex-row lg:gap-0 lg:text-sm">
+                <ul className="flex flex-col gap-2 font-medium tracking-normal lg:flex-row lg:gap-0 ">
                   {[
                     {
                       label: "Features",
                       href: "#features",
-                    },
-                    {
-                      label: "Solution",
-                      href: "#solution",
                     },
                   ].map(({ label, href }) => (
                     <li
@@ -84,7 +79,7 @@ export default function Header() {
                     >
                       <a
                         href={href}
-                        className="block transition hover:text-brand dark:hover:text-brand-400 md:px-4"
+                        className="block text-gray-600 transition hover:text-brand dark:hover:text-brand-400 md:px-4"
                       >
                         <span>{label}</span>
                       </a>
@@ -102,9 +97,9 @@ export default function Header() {
                 </ul>
               </div>
 
-              <div className="mr-2">
+              {/* <div className="mr-2">
                 <ThemeToggle />
-              </div>
+              </div> */}
 
               <div className="mt-8 w-full sm:w-auto lg:mt-0">
                 <ButtonLink
