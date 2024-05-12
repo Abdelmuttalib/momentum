3fab85a5e600e5ad450b48fd5b348d5e13cea29a
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2024-05-13 02:32:29 +0800

    refactor: Preload fonts for improved performance

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,36 +1,34 @@
-import {
-  // ArrowLeftIcon,
-  HomeIcon,
-} from "@heroicons/react/20/solid";
+import { HomeIcon } from "@heroicons/react/20/solid";
 import * as React from "react";
 
 import Seo from "@/components/Seo";
-import { Layout } from "@/components/layout";
-import Link from "next/link";
 import { cn } from "@/utils/cn";
-import {
-  ButtonLink,
-  // Button,
-  buttonVariants,
-} from "@/components/ui/button";
-// import { useRouter } from "next/router";
+import { ButtonLink } from "@/components/ui/button";
+import { Typography } from "@/components/ui/typography";
 
 export default function NotFoundPage() {
-  // const { back } = useRouter();
-
   return (
     <>
       <Seo templateTitle="404 | Not Found" />
 
       <main className="flex h-full min-h-screen w-full items-center justify-center px-6 py-24 sm:py-32 lg:px-6">
         <div className="text-center">
-          <p className="text-3xl font-semibold text-brand">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          <Typography
+            as="p"
+            variant="display-sm/semibold"
+            className="text-primary"
+          >
+            404
+          </Typography>
+          <Typography as="h1" variant="display-lg/semibold" className="mt-4">
             Page not found
-          </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">
-            Sorry, we couldn’t find the page you’re looking for.
-          </p>
+          </Typography>
+          <Typography
+            as="p"
+            className="mt-6 text-base leading-7 text-foreground-lighter"
+          >
+            Sorry, we couldn&apos;t find the page you&apos;re looking for.
+          </Typography>
           <div className="mt-10 flex justify-center gap-x-3">
             {/* <Button
               onClick={() => back()}
@@ -43,10 +41,9 @@ export default function NotFoundPage() {
             <ButtonLink
               href="/"
               size="lg"
-              className={cn("inline-flex items-center justify-center gap-1.5")}
+              leftIcon={<HomeIcon className="mb-0.5 w-5" />}
             >
-              <HomeIcon className="mb-0.5 w-5" />
-              <span>Home</span>
+              Home
             </ButtonLink>
           </div>
         </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,36 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+
+        {/* preload all fonts */}
+        <link
+          rel="preload"
+          href="/fonts/inter-var-latin.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=satoshi@300,301,400,401,500,501,700,701,900,901&f[]=switzer@100,101,200,201,300,301,400,401,500,501,600,601,700,701,800,801,900,901&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          rel="preload"
+          href="/fonts/PlusJakartaSans-VariableFont_wght.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+
+        <link
+          rel="preload"
+          href="/fonts/Onest-VariableFont_wght.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
