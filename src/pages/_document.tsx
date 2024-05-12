f8e2e672070c86efdd681f421cc316ee9245bbb9
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2024-05-13 02:58:59 +0800

    refactor: Remove unused font stylesheet in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,11 +16,6 @@ export default function Document() {
         />
 
         <link
-          href="https://api.fontshare.com/v2/css?f[]=satoshi@300,301,400,401,500,501,700,701,900,901&f[]=switzer@100,101,200,201,300,301,400,401,500,501,600,601,700,701,800,801,900,901&display=swap"
-          rel="stylesheet"
-        />
-
-        <link
           rel="preload"
           href="/fonts/PlusJakartaSans-VariableFont_wght.ttf"
           as="font"
